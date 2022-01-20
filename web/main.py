802d051d8ede8ae3038b275fce54168e663effea
SgOm <juj2660@naver.com>
2022-01-20 02:37:12 -0500

    add asset

--- web/main.py
+++ web/main.py
@@ -11,7 +11,12 @@ p = Process(target = channel_hopper)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/address")
+def address():
+    return render_template("address.html")
+@app.route("/signal")
+def signalpage():
+    return render_template("signal.html")
 @app.route("/ap")
 def getAp():
     return "getAP"
